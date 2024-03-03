556ef7e50d6202f2ec2f73a850c9f5af4b22512d
Nada-Hany <nadaaayman993@gmail.com>
2024-03-03 14:31:09 +0200

    first version of tic tac toe

--- TicTacToeGame/app/src/main/java/com/example/tictactoe_game/MainActivity.kt
+++ TicTacToeGame/app/src/main/java/com/example/tictactoe_game/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.example.tictactoe_game
-
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Range
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
+import android.annotation.SuppressLint
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     private lateinit var cell00:ImageView
@@ -17,15 +19,13 @@ class MainActivity : AppCompatActivity() {
     private lateinit var cell20:ImageView
     private lateinit var cell21:ImageView
     private lateinit var cell22:ImageView
-    lateinit var playerTurn:TextView
+    lateinit var playerTurnText:TextView
     lateinit var winner:TextView
+    lateinit var playAgianButton :Button
     private var xTurn = true
+    var gameDone = false
 
-
-    val isPressed = arrayListOf<Array<Boolean>>(
-                arrayOf(false, false, false),
-        arrayOf(false, false, false),
-        arrayOf(false, false, false))
+    val inCell : MutableList<MutableList<String>> = mutableListOf()
 
     val images:MutableList<MutableList<ImageView>> = mutableListOf()
 
@@ -48,40 +48,70 @@ class MainActivity : AppCompatActivity() {
         images.add(0,mutableListOf(cell00, cell01, cell02))
         images.add(1,mutableListOf(cell10, cell11, cell12))
         images.add(2,mutableListOf(cell20, cell21, cell22))
-        playerTurn = findViewById(R.id.playerTurn)
+        inCell.add(0, mutableListOf("null", "null", "null"))
+        inCell.add(1, mutableListOf("null", "null", "null"))
+        inCell.add(2, mutableListOf("null", "null", "null"))
+
+        playerTurnText = findViewById(R.id.playerTurn)
+        playAgianButton = findViewById(R.id.playAgain)
         winner = findViewById(R.id.winner)
     }
 
+    @SuppressLint("SetTextI18n")
     fun setXO(x:Int, y:Int){
-        if(!isPressed[x][y]){
-            if(xTurn) {
-                Toast.makeText(this, "true", Toast.LENGTH_LONG).show()
-                images[x][y].setImageResource(R.drawable.x)
-                isPressed[x][y] = true
+        if(!gameDone) {
+            if (inCell[x][y] == "null") {
+                if (xTurn) {
+                    images[x][y].setImageResource(R.drawable.x)
+                    inCell[x][y] = "x"
+                } else {
+                    images[x][y].setImageResource(R.drawable.o)
+                    inCell[x][y] = "o"
+                }
+                changeTurns()
+                if (checkForWinners(x, y, inCell[x][y])) {
+                    gameDone = true
+                    winner.setText("${inCell[x][y]} won!")
+                    playerTurnText.setText("")
+                    playAgianButton.visibility = View.VISIBLE
+                }
             }
-            else {
-                images[x][y].setImageResource(R.drawable.o)
-                isPressed[x][y] = true
-            }
-            changeTurns()
         }
     }
-
     fun changeTurns(){
         if (xTurn) {
             xTurn = false
-            playerTurn.setText("O turn")
+            playerTurnText.setText("O turn")
         }
         else {
             xTurn = true
-            playerTurn.setText("X turn")
+            playerTurnText.setText("X turn")
         }
     }
 
-    fun checkForWinners(){
-
+    fun checkForWinners(x:Int, y:Int, patternPlayed:String):Boolean{
+        //row
+        if(inCell[x][0] == patternPlayed && inCell[x][1] == inCell[x][0] && inCell[x][1] ==inCell[x][2])
+            return true
+        //col
+        if(inCell[0][y] == patternPlayed && inCell[0][y] == inCell[1][y] && inCell[1][y] ==inCell[2][y])
+            return true
+        //main/primarily diagonal
+        if(x==y){
+            if ((inCell[0][0]==inCell[1][1] && inCell[1][1]==inCell[2][2] && inCell[1][1] == patternPlayed))
+               return true
+        }
+        //secondary diagonal
+        if(y==2-x) {
+            if ((inCell[0][2] == inCell[1][1] && inCell[1][1] == inCell[2][0] && inCell[1][1] == patternPlayed))
+                return true
+        }
+        return false
     }
 
+    fun checkForDraw(){
+
+    }
     fun pressed22(view: View) {
         setXO( 2, 2)
     }
@@ -109,4 +139,23 @@ class MainActivity : AppCompatActivity() {
     fun pressed00(view: View) {
         setXO(0, 0)
     }
+
+    fun playAgain(view: View) {
+        for (row in images){
+            for (cell in row){
+                cell.setImageResource(R.drawable.empty)
+            }
+        }
+        inCell.clear()
+        inCell.add(0, mutableListOf("null", "null", "null"))
+        inCell.add(1, mutableListOf("null", "null", "null"))
+        inCell.add(2, mutableListOf("null", "null", "null"))
+        gameDone = false
+        xTurn = true
+        playAgianButton.visibility = View.INVISIBLE
+        winner.setText("")
+        playerTurnText.text = "X turn"
+    }
 }
+
+
